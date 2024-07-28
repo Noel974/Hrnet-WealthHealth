fea19dec8ff4f24d7ceb6af14e1e44008f72629c
Noel974 <noelantoine974@outlook.fr>
2024-07-28 16:51:41 +0400

    correction jsdoc

--- Modaleon/dist/Components/Modal.d.ts
+++ Modaleon/dist/Components/Modal.d.ts
@@ -1,5 +1,13 @@
 import React, { ReactNode } from 'react';
 import './Modal.css';
+/**
+ * Composant de fenêtre modale.
+ * @param {Object} props - Propriétés du composant.
+ * @param {boolean} props.isOpen - Indique si la modal est ouverte.
+ * @param {Function} props.onClose - Fonction de rappel pour fermer la modal.
+ * @param {ReactNode} props.children - Éléments enfants à afficher dans la modal.
+ * @returns {JSX.Element|null} - Composant rendu ou null si la modal n'est pas ouverte.
+ */
 interface ModalProps {
     isOpen: boolean;
     onClose: () => void;
--- Modaleon/dist/Components/Modal.js
+++ Modaleon/dist/Components/Modal.js
@@ -7,11 +7,23 @@ exports["default"] = void 0;
 var _react = _interopRequireDefault(require("react"));
 require("./Modal.css");
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+/**
+ * Composant de fenêtre modale.
+ * @param {Object} props - Propriétés du composant.
+ * @param {boolean} props.isOpen - Indique si la modal est ouverte.
+ * @param {Function} props.onClose - Fonction de rappel pour fermer la modal.
+ * @param {ReactNode} props.children - Éléments enfants à afficher dans la modal.
+ * @returns {JSX.Element|null} - Composant rendu ou null si la modal n'est pas ouverte.
+ */
+
+// Interface pour les propriétés d'un composant Modal dans React
+
 var Modal = function Modal(_ref) {
   var isOpen = _ref.isOpen,
     onClose = _ref.onClose,
     children = _ref.children;
-  if (!isOpen) return null;
+  if (!isOpen) return null; // Si la modal n'est pas ouverte, ne rend rien
+
   return /*#__PURE__*/_react["default"].createElement("div", {
     className: "modal-overlay"
   }, /*#__PURE__*/_react["default"].createElement("div", {
@@ -19,8 +31,8 @@ var Modal = function Modal(_ref) {
   }, /*#__PURE__*/_react["default"].createElement("button", {
     className: "modal-close",
     onClick: onClose
-  }, "\xD7"), /*#__PURE__*/_react["default"].createElement("div", {
+  }, "\xD7 "), /*#__PURE__*/_react["default"].createElement("div", {
     className: "body"
-  }, children)));
+  }, children, " ")));
 };
 var _default = exports["default"] = Modal;
